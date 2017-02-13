f4c1db683a236bb09690c4de594f03e8e27eab4b
Marvin Oey <moey@usgs.gov>
2017-02-13 15:29:49 -0500

    Add templating service

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -3,22 +3,14 @@
 namespace Yoda\EventBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction($count, $firstName)
     {
-      $data = array(
-        'count' => $count,
-        'firstName' => $firstName,
-        'ackbar' => 'It\'s a traaaaaaaaaaap.',
+      return $this->render(
+        'EventBundle:Default:index.html.twig',
+        array('name' => $firstName)
       );
-      $json = json_encode($data);
-
-      $response = new JsonResponse($json);
-
-      return $response;
     }
 }
